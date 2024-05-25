4b6f91d82fa4224ed9c78a592a330ea4e689fb50
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-05-25 14:05:02 -0300

    feat: metada on already created pages

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Login",
+    description: "Comece agora e faça seu login!",
+  };
+  
+
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
     return (
         <section>
